531df80221c26e7ceab857bb3196d440b80d038e
pconsag <pconsagra@g.ucla.edu>
2021-06-24 17:35:54 -0700

    add yet another js file

--- Develop/script3.js
+++ Develop/script3.js
@@ -1,28 +1,47 @@
+
 var passwordLength
 var userNumbers
 var userLowercase
 var userUppercase
 var userSpecial
-var lowercase = ("abcdefghijklmnopqrstuvwxyz");
-var uppercase= ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-var numbers= ("1234567890");
-// var special= (" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
-var pressed = document.querySelector("#generate")
+var lowercase = ("abcdefghijklmnopqrstuvwxyz")
+var uppercase= ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var numbers= ("1234567890")
 
 
-function userLength () { 
+
+function userLength() {{
     passwordLength = prompt("how many characters would you like in your password? (must be between 8-128");
-    console.log(passwordLength);
-    return passwordLength;
-}
-
-function wantNumbers() {
-    if (passwordLength >=8 && passwordLength<=128 );{
-    userNumbers = confirm("would you like numbers in your password?"); 
-        }
-    else {
-        altert("please pick a number between 8-128")
+    console.log(passwordLength)}{
     }
-}
+    if (passwordLength >=8 && passwordLength<=128 ) {
+    
+     
+    };
 
-pressed.addEventListener("click", userLength)
+
+
+
+function Numbers() {
+    userNumbers = confirm("would you like numbers in your password?") 
+      }
+
+
+
+  
+
+
+
+
+// if (passwordLength >=8 && passwordLength<=128 );{
+//     userNumbers = confirm("would you like numbers in your password?") 
+//       }
+//       if (userNumbers === true); {
+//         userLowercase = confirm("would you like lower case in your password?") 
+//       }
+//       if (userLowercase === true); {
+//         userUpperrcase = confirm("would you like upper case in your password?") 
+//       }
+//       if (userUppercase === true); {
+//         userSpecial = confirm("would you like special characters in your password?") 
+//       }
